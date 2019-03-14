18a9fba046a3fa9082eaeb0a1388613625be6273
caihy <228417442@qq.com>
2019-03-14 11:29:25 +0800

    修改上传和下载

--- s3sync.go
+++ s3sync.go
@@ -10,42 +10,42 @@ import (
 
 func main() {
 	synctime := flag.Int("sync-time", 30, "sync time defualt 30s")
-	pullsourcedir := flag.String("pull-source-dir", "s3://dsdata01/rec_plt_svr/base_pull", "aws s3 pull dir")
-	pulltargetdir := flag.String("pull-target-dir", "/data/base_pull", "aws s3 pull local dir")
-	pushsourcedir := flag.String("push-source-dir", "/data/base_push", "aws s3 push local dir")
-	pushtargetdir := flag.String("push-target-dir", "s3://dsdata01/rec_plt_svr/base_pull", "aws s3 push dir")
+	pullsourcedir := flag.String("pull-source-dir", "", "aws s3 pull dir")
+	pulltargetdir := flag.String("pull-target-dir", "", "aws s3 pull local dir")
+	pushsourcedir := flag.String("push-source-dir", "", "aws s3 push local dir")
+	pushtargetdir := flag.String("push-target-dir", "", "aws s3 push dir")
 
 	flag.Parse()
-	if *pushsourcedir != "/data/base_push" {
-		if *pushtargetdir != "s3://dsdata01/rec_plt_svr/base_pull" {
-			for {
-				timeStr := time.Now().Format("2006-01-02 15:04:05")
-				fmt.Println("当前时间：", timeStr, "同步间隔时间（s）：", *synctime)
-				st := time.Duration(*synctime)
-				time.Sleep(st * time.Second)
-				runcmdpullandpush(*pushsourcedir, *pushtargetdir, *pullsourcedir, *pulltargetdir)
-			}
-		} else {
-			log.Fatal("没有目的路径")
+	if *pushsourcedir != "" && *pushtargetdir != "" && *pullsourcedir != "" && *pulltargetdir != "" {
+		for {
+			timeStr := time.Now().Format("2006-01-02 15:04:05")
+			fmt.Println("当前时间：", timeStr, "同步间隔时间（s）：", *synctime)
+			st := time.Duration(*synctime)
+			time.Sleep(st * time.Second)
+			runcmdpull(*pullsourcedir, *pulltargetdir)
+			runcmdpush(*pushsourcedir, *pushtargetdir)
 		}
-	} else if *pullsourcedir != "s3://dsdata01/rec_plt_svr/base_pull" {
-		if *pulltargetdir != "/data/base_pull" {
-			for {
-				timeStr := time.Now().Format("2006-01-02 15:04:05")
-				fmt.Println("当前时间：", timeStr, "同步间隔时间（s）：", *synctime)
-				st := time.Duration(*synctime)
-				time.Sleep(st * time.Second)
-				runcmdpull(*pullsourcedir, *pulltargetdir)
-			}
-		} else {
-			log.Fatal("没有目的路径")
+	} else if *pullsourcedir != "" && *pulltargetdir != "" && *pushsourcedir == "" && *pushtargetdir == "" {
+		for {
+			timeStr := time.Now().Format("2006-01-02 15:04:05")
+			fmt.Println("当前时间：", timeStr, "同步间隔时间（s）：", *synctime)
+			st := time.Duration(*synctime)
+			time.Sleep(st * time.Second)
+			runcmdpull(*pullsourcedir, *pulltargetdir)
+		}
+	} else if *pullsourcedir == "" && *pulltargetdir == "" && *pushsourcedir != "" && *pushtargetdir != "" {
+		for {
+			timeStr := time.Now().Format("2006-01-02 15:04:05")
+			fmt.Println("当前时间：", timeStr, "同步间隔时间（s）：", *synctime)
+			st := time.Duration(*synctime)
+			time.Sleep(st * time.Second)
+			runcmdpush(*pullsourcedir, *pulltargetdir)
 		}
 	} else {
-		log.Fatal("缺少源路径")
+		log.Fatal("路径不能为空")
 	}
 }
-func runcmdpullandpush(pushsourcedir string, pushtargetdir string, pullsourcedir string, pulltargetdir string) {
-	runcmdpull(pullsourcedir, pulltargetdir)
+func runcmdpush(pushsourcedir, pushtargetdir string) {
 	//time.Sleep(2 * time.Second)
 	cmd := exec.Command("aws", "s3", "sync", pushsourcedir, pushtargetdir, "--delete")
 
@@ -53,13 +53,13 @@ func runcmdpullandpush(pushsourcedir string, pushtargetdir string, pullsourcedir
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
-	
+
 	if err := cmd.Wait(); err != nil {
 		fmt.Println("wait:", err.Error())
-    }
-	fmt.Println("pull源路径：", pullsourcedir, "pull目标路径：", pulltargetdir, "push源路径：", pushsourcedir, "push目标路径：", pushtargetdir)
+	}
+	fmt.Println("push源路径：", pushsourcedir, "push目标路径：", pushtargetdir)
 }
-func runcmdpull(pullsourcedir string, pulltargetdir string) {
+func runcmdpull(pullsourcedir, pulltargetdir string) {
 	cmd := exec.Command("aws", "s3", "sync", pullsourcedir, pulltargetdir, "--exact-timestamps", "--delete")
 	// 运行命令
 	if err := cmd.Start(); err != nil {
@@ -67,6 +67,6 @@ func runcmdpull(pullsourcedir string, pulltargetdir string) {
 	}
 	if err := cmd.Wait(); err != nil {
 		fmt.Println("wait:", err.Error())
-    }
+	}
 	fmt.Println("pull源路径：", pullsourcedir, "pull目标路径：", pulltargetdir)
 }
